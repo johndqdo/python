658688995cfa95168792aebf8d443ee76d2a817d
João Victor Santana Ferreira <joaovictorsantanaferreira@gmail.com>
2022-08-03 09:27:54 -0300

    Capaz de soma, multiplicação, divisão e subtração

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+print("Calculadora simples, digite um número, depois +,-,/,*, depois outro número. Escreva 'done' quando quiser finalizar")
+r = input()
+r = float(r)
+while True:
+    opcao = input()
+    if opcao == '+':
+        n = input()
+        n = float(n)
+        r += n
+        print(r)
+    elif opcao == 'done':
+        quit()
+    elif opcao.isdigit():
+        r = float(opcao)
+        print(r)
+    elif opcao == '-':
+        n = input()
+        n = float(n)
+        r -= n
+        print(r)
+    elif opcao == '*':
+        n = input()
+        n = float(n)
+        r *= n
+        print(r)
+    elif opcao == '/':
+        n = input()
+        n = float(n)
+        r /= n
+        print(r)
