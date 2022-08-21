ab7644e0d568be72b042ab41da6a81c34533ef76
João Victor Santana Ferreira <joaovictorsantanaferreira@gmail.com>
2022-08-21 19:24:24 -0300

    Corrigido erros de port, adic exceções, aumento na quant de vezes em que done pode ser utilizado

--- calculadora.py
+++ calculadora.py
@@ -1,30 +1,60 @@
 print("Calculadora simples, digite um número, depois +,-,/,*, depois outro número. Escreva 'done' quando quiser finalizar")
-r = input()
-r = float(r)
+numPrincipal = input("Digite um número inicial\n")
 while True:
-    opcao = input()
-    if opcao == 'done':
-        quit()
-    elif opcao.isdigit():
-        r = float(opcao)
-        print(r)
-    elif opcao == '+':
-        n = input()
-        n = float(n)
-        r += n
-        print(r)
-    elif opcao == '-':
-        n = input()
-        n = float(n)
-        r -= n
-        print(r)
-    elif opcao == '*':
-        n = input()
-        n = float(n)
-        r *= n
-        print(r)
-    elif opcao == '/':
-        n = input()
-        n = float(n)
-        r /= n
-        print(r)
+    try:
+        numPrincipal = float(numPrincipal)
+    except:
+        print("Digito inválido")
+        numPrincipal = input()
+        continue
+    while True:
+        opcao = input("Escolhar a operação que deseja(+, -, *, /) ou escolha outro número\n")
+        if opcao == 'done':
+            quit()
+        elif opcao.isdigit():
+            numPrincipal = float(opcao)
+            print(numPrincipal)
+        elif opcao == '+':
+            numSecundario = input()
+            if numSecundario == 'done':
+                quit()
+            try:
+                numSecundario = float(numSecundario)
+            except:
+                print("Digito Inválido")
+                continue
+            numPrincipal += numSecundario
+            print(numPrincipal)
+        elif opcao == '-':
+            numSecundario = input()
+            if numSecundario == 'done':
+                quit()
+            try:
+                numSecundario = float(numSecundario)
+            except:
+                print("Digito Inválido")
+                continue
+            numPrincipal -= numSecundario
+            print(numPrincipal)
+        elif opcao == '*':
+            numSecundario = input()
+            if numSecundario == 'done':
+                quit()
+            try:
+                numSecundario = float(numSecundario)
+            except:
+                print("Digito Inválido")
+                continue
+            numPrincipal *= numSecundario
+            print(numPrincipal)
+        elif opcao == '/':
+            numSecundario = input()
+            if numSecundario == 'done':
+                quit()
+            try:
+                numSecundario = float(numSecundario)
+            except:
+                print("Digito Inválido")
+                continue
+            numPrincipal /= numSecundario
+            print(numPrincipal)
